e650d71d722728e33d9c34108bd60a6f02602d78
EvanWhite <78002495+ImBadAtCodingStuff@users.noreply.github.com>
2024-02-24 19:25:18 -0800

    this shouldve been pushed sooner lol

---
+++ Drivetrain/drive.py
@@ -0,0 +1,18 @@
+
+# a subdirectory for controlling subsystems
+
+
+
+# controls the servo to turn the fron tires
+def turn():
+    pass
+
+# sets the motor voltage to positive 1/whatever voltage is a good speed
+def forward(vari):
+    # set the motor speed to 1
+    pass
+
+# sets the motor voltage to negative 1/whatever voltage is a good speed
+def reverse():
+    # set motor speed to -1
+    pass
---
+++ main.py
@@ -0,0 +1,8 @@
+
+import Drivetrain.drive as Drivetrain
+from time import sleep
+
+
+Drivetrain.forward(1)
+sleep(2)
+Drivetrain.forward(0)
